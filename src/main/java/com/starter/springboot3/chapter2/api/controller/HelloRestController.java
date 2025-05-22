f1d7d6e3095a245380a020edc44870b4328324ac
JuJin1324 <jujin1324@daum.net>
2025-05-22 19:41:23 +0900

    feat: GraalVM 테스트 패키지 이동

--- src/main/java/com/starter/springboot3/chapter2/api/controller/HelloRestController.java
+++ src/main/java/com/starter/springboot3/chapter2/api/controller/HelloRestController.java
@@ -1,4 +1,4 @@
-package com.starter.springboot3.api.controller;
+package com.starter.springboot3.chapter2.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
